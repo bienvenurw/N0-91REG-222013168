a4f5eb4859d7682c54c02f1169ae1d2284d9534c
bienvenurw <mwumvanezabienvenu@gmail.com>
2023-11-04 09:05:40 +0200

    Editing file
    Edited filles

---
+++ bbb.java
@@ -0,0 +1,22 @@
+public class bbb{  
+public static void main(String args[])
+{
+    int rwanda_area = 2004;
+    
+    if(rwanda_area > 200)
+    {
+        System.out.println("congo");
+    } else if (rwanda_area < 150)
+    {
+        System.out.println("kigali");
+
+    }else if(rwanda_area == 2004)
+    {
+        System.out.println("kigali rwanda");
+
+}else if(rwanda_area <= 200)
+{ 
+    System.out.println("kagame");
+}
+}
+}
